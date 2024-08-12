d8f81925efe68b72fd34e6de7e596b83ae3bdaaa
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-08-12 14:36:31 +0530

    feat: setup basic left side panel

---
+++ frontend/src/components/StudioLeftPanel.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="flex flex-row shadow-lg z-5 overflow-auto">
+		<!-- Primary Menu -->
+		<div class="relative flex flex-col space-y-2 bg-white w-12 p-2 h-full border-r border-gray-200">
+			<div
+				class="flex items-center"
+				v-for="tab in sidebarMenu"
+				:key="tab.label"
+				@click="setActiveTab(tab.label)"
+			>
+				<Tooltip placement="right" :text="tab.label" :hover-delay="0.1">
+					<div
+						class="flex items-center justify-center cursor-pointer gap-2 truncate rounded px-3 py-1 transition duration-300 ease-in-out"
+						:class="{ 'bg-gray-100': activeTab === tab.label }"
+					>
+						<FeatherIcon :name="tab.icon" class="h-6 w-6 text-gray-600" />
+					</div>
+				</Tooltip>
+			</div>
+		</div>
+
+		<!-- Secondary Menu -->
+		<div :style="{ width: `${280 - 48}px` }">
+			<div class="text-base font-semibold text-gray-800 p-3 border-b-[1px] border-gray-200">
+				{{ activeTab }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import { Tooltip, FeatherIcon } from "frappe-ui"
+
+const activeTab = ref("Add Component")
+const sidebarMenu = [
+	{
+		label: "Add Component",
+		icon: "plus-circle",
+	},
+	{
+		label: "Layers",
+		icon: "layers",
+	},
+	{
+		label: "Data",
+		icon: "database",
+	},
+	{
+		label: "Code",
+		icon: "code",
+	},
+]
+
+const setActiveTab = (tab) => {
+	activeTab.value = tab
+}
+</script>
--- frontend/src/pages/StudioPage.vue
+++ frontend/src/pages/StudioPage.vue
@@ -1,17 +1,18 @@
 <template>
 	<div class="studio h-screen flex-col overflow-hidden bg-gray-100">
-		<StudioToolbar />
-		<div>
+		<StudioToolbar class="relative z-30" />
+		<div class="flex flex-col">
+			<StudioLeftPanel class="absolute bottom-0 left-0 top-[var(--toolbar-height)] bg-white" />
 			<StudioCanvas
 				class="canvas-container absolute bottom-0 top-[var(--toolbar-height)] flex justify-center overflow-hidden bg-gray-200 p-10 left-[280px] right-[275px]"
-			>
-			</StudioCanvas>
+			/>
 		</div>
 	</div>
 </template>
 
 <script setup>
 import StudioToolbar from "@/components/StudioToolbar.vue"
+import StudioLeftPanel from "@/components/StudioLeftPanel.vue"
 import StudioCanvas from "@/components/StudioCanvas.vue"
 </script>
 
