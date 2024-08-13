d885054ca82bbbed132f24913e429655a3a1b672
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-08-13 07:34:53 +0530

    feat: add LucideIcon component

---
+++ frontend/src/components/LucideIcon.vue
@@ -0,0 +1,19 @@
+<template>
+	<component :is="icon" />
+</template>
+
+<script setup>
+import { computed } from "vue"
+// NOTE: Find a better way than importing the entire icon library -> increased bundle size
+// Timeless icons will replace these most likely
+import * as icons from "lucide-vue-next"
+
+const props = defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+})
+
+const icon = computed(() => icons[props.name])
+</script>
