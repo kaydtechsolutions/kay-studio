2a7915e67ead1dcb4e797b2698841cba28a7c31b
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-08-18 22:55:13 +0530

    feat: add initial state for listview component

--- frontend/src/data/components.js
+++ frontend/src/data/components.js
@@ -1,3 +1,6 @@
+import { h } from "vue"
+import { Avatar } from "frappe-ui"
+
 export const COMPONENTS = {
 	Alert: {
 		name: "Alert",
@@ -71,6 +74,60 @@ export const COMPONENTS = {
 		name: "ListView",
 		title: "List View",
 		icon: "ListCheck",
+		initialState: {
+			columns: [
+				{
+					label: "Name",
+					key: "name",
+					width: 3,
+					getLabel: ({ row }) => row.name,
+					prefix: ({ row }) => {
+						return h(Avatar, {
+							shape: "circle",
+							image: row.user_image,
+							size: "sm",
+						})
+					},
+				},
+				{
+					label: "Email",
+					key: "email",
+					width: "200px",
+				},
+				{
+					label: "Role",
+					key: "role",
+				},
+				{
+					label: "Status",
+					key: "status",
+				},
+			],
+			rows: [
+				{
+					id: 1,
+					name: "John Doe",
+					email: "john@doe.com",
+					status: "Active",
+					role: "Developer",
+					user_image: "https://avatars.githubusercontent.com/u/499550",
+				},
+				{
+					id: 2,
+					name: "Jane Doe",
+					email: "jane@doe.com",
+					status: "Inactive",
+					role: "HR",
+					user_image: "https://avatars.githubusercontent.com/u/499120",
+				},
+			],
+			rowKey: "id",
+			options: {
+				selectable: true,
+				showTooltip: true,
+				resizeColumn: true,
+			},
+		},
 	},
 }
 
