983f621d452ed86e661d2fa08780cfc2a4305e43
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-09-01 20:18:19 +0530

    feat: wire context menu on layers panel

--- frontend/src/components/ComponentLayers.vue
+++ frontend/src/components/ComponentLayers.vue
@@ -8,57 +8,61 @@
 			:disabled="blocks.length && blocks[0].isRoot()"
 		>
 			<template #item="{ element }">
-				<div
-					:data-component-layer-id="element.componentId"
-					:title="element.componentId"
-					class="dark:bg-zinc-900 min-w-24 cursor-pointer overflow-hidden rounded border border-transparent bg-white bg-opacity-50 text-base text-gray-700 dark:text-gray-500"
-					@click.stop="store.selectBlock(element, $event, false)"
-					@mouseover.stop="store.hoveredBlock = element.componentId"
-					@mouseleave="store.hoveredBlock = null"
-				>
-					<span
-						class="group my-[7px] flex items-center gap-1.5 pr-[2px] font-medium"
-						:style="{ paddingLeft: `${indent}px` }"
-						:class="{
-							'!opacity-50': !element.isVisible(),
-						}"
-					>
-						<FeatherIcon
-							v-if="element.children && element.children.length && !element.isRoot()"
-							:name="isExpanded(element) ? 'chevron-down' : 'chevron-right'"
-							class="h-3 w-3"
-							@click.stop="toggleExpanded(element)"
-						/>
-						<LucideIcon :name="element.getIcon()" class="h-3 w-3" />
-
-						<span
-							class="min-h-[1em] min-w-[2em] truncate"
-							:contenteditable="element.editable"
-							:title="element.blockId"
-							@dblclick="element.editable = true"
-							@keydown.enter.stop.prevent="element.editable = false"
-							@blur="setBlockName($event, element)"
+				<div>
+					<ComponentContextMenu v-slot="{ onContextMenu }" :block="element" :editable="false">
+						<div
+							:data-component-layer-id="element.componentId"
+							:title="element.componentId"
+							class="dark:bg-zinc-900 min-w-24 cursor-pointer overflow-hidden rounded border border-transparent bg-white bg-opacity-50 text-base text-gray-700 dark:text-gray-500"
+							@contextmenu.prevent.stop="onContextMenu"
+							@click.stop="store.selectBlock(element, $event, false)"
+							@mouseover.stop="store.hoveredBlock = element.componentId"
+							@mouseleave="store.hoveredBlock = null"
 						>
-							{{ element.getBlockDescription() }}
-						</span>
+							<span
+								class="group my-[7px] flex items-center gap-1.5 pr-[2px] font-medium"
+								:style="{ paddingLeft: `${indent}px` }"
+								:class="{
+									'!opacity-50': !element.isVisible(),
+								}"
+							>
+								<FeatherIcon
+									v-if="element.children && element.children.length && !element.isRoot()"
+									:name="isExpanded(element) ? 'chevron-down' : 'chevron-right'"
+									class="h-3 w-3"
+									@click.stop="toggleExpanded(element)"
+								/>
+								<LucideIcon :name="element.getIcon()" class="h-3 w-3" />
+								<span
+									class="min-h-[1em] min-w-[2em] truncate"
+									:contenteditable="element.editable"
+									:title="element.blockId"
+									@dblclick="element.editable = true"
+									@keydown.enter.stop.prevent="element.editable = false"
+									@blur="setBlockName($event, element)"
+									>
+									{{ element.getBlockDescription() }}
+								</span>
 
-						<!-- toggle visibility -->
-						<FeatherIcon
-							v-if="!element.isRoot()"
-							:name="element.isVisible() ? 'eye' : 'eye-off'"
-							class="ml-auto mr-2 hidden h-3 w-3 group-hover:block"
-							@click.stop="element.toggleVisibility()"
-						/>
-						<span
-							v-if="element.isRoot()"
-							class="dark:text-zinc-500 ml-auto mr-2 text-sm capitalize text-gray-500"
-						>
-							{{ store.activeBreakpoint }}
-						</span>
-					</span>
-					<div v-show="canShowChildLayer(element)">
-						<ComponentLayers :blocks="element.children" ref="childLayer" :indent="childIndent" />
-					</div>
+								<!-- toggle visibility -->
+								<FeatherIcon
+									v-if="!element.isRoot()"
+									:name="element.isVisible() ? 'eye' : 'eye-off'"
+									class="ml-auto mr-2 hidden h-3 w-3 group-hover:block"
+									@click.stop="element.toggleVisibility()"
+								/>
+								<span
+									v-if="element.isRoot()"
+									class="dark:text-zinc-500 ml-auto mr-2 text-sm capitalize text-gray-500"
+								>
+								{{ store.activeBreakpoint }}
+								</span>
+							</span>
+							<div v-show="canShowChildLayer(element)">
+								<ComponentLayers :blocks="element.children" ref="childLayer" :indent="childIndent" />
+							</div>
+						</div>
+					</ComponentContextMenu>
 				</div>
 			</template>
 		</Draggable>
@@ -71,6 +75,7 @@ import { FeatherIcon } from "frappe-ui"
 import Draggable from "vuedraggable"
 
 import ComponentLayers from "@/components/ComponentLayers.vue"
+import ComponentContextMenu from "@/components/ComponentContextMenu.vue"
 
 import useStore from "@/store"
 import Block from "@/utils/block"
