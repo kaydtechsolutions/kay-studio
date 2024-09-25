0de7d3d4281ec71bad76286584aa9cd54dba859f
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-09-25 15:27:06 +0530

    refactor: move resource manager to data panel to remove the additional component overhead

--- frontend/src/components/DataPanel.vue
+++ frontend/src/components/DataPanel.vue
@@ -8,18 +8,70 @@
 			</div>
 
 			<EmptyState v-else message="No resources added" />
-			<PageResourceManager v-if="store.activePage" :page="store.activePage" />
+
+			<div class="flex flex-col" v-if="store.activePage">
+				<Button @click="showAddResourceDialog = true">Add Resource</Button>
+				<ResourceDialog v-model:showDialog="showAddResourceDialog" @addResource="addResource" />
+			</div>
 		</CollapsibleSection>
 	</div>
 </template>
 
 <script setup>
+import { ref } from "vue"
 import useStudioStore from "@/stores/studioStore"
 import CollapsibleSection from "@/components/CollapsibleSection.vue"
-import PageResourceManager from "@/components/PageResourceManager.vue"
 import ObjectBrowser from "@/components/ObjectBrowser.vue"
 import EmptyState from "@/components/EmptyState.vue"
-import { isObjectEmpty } from "@/utils/helpers"
+import ResourceDialog from "@/components/ResourceDialog.vue"
+
+import { isObjectEmpty, getAutocompleteValues } from "@/utils/helpers"
+import { studioResources, studioPageResources } from "@/data/studioResources"
+
+/**
+ * Insert resource into DB
+ * Attach resource to page
+ * fetch resources attached to page in store
+ * show resources on the data panel
+ */
 
 const store = useStudioStore()
+const showAddResourceDialog = ref(false)
+
+const attachResource = async (resource) => {
+	studioPageResources.insert
+		.submit({
+			studio_resource: resource.name,
+			parent: store.activePage?.name,
+			parenttype: "Studio Page",
+			parentfield: "resources",
+		})
+		.then(async () => {
+			await store.setPageResources(store.activePage)
+			showAddResourceDialog.value = false
+		})
+}
+
+const addResource = (resource) => {
+	if (resource.source === "Existing Resource") {
+		attachResource(resource)
+		return
+	}
+	const fields = getAutocompleteValues(resource.fields)
+
+	studioResources.insert
+		.submit({
+			name: resource.name,
+			resource_type: resource.resource_type,
+			document_type: resource.document_type,
+			document_name: resource.document_name,
+			url: resource.url,
+			method: resource.method,
+			fields: fields,
+		})
+		.then((res) => {
+			studioPageResources.filters = { parent: store.activePage?.name }
+			attachResource(res)
+		})
+}
 </script>
