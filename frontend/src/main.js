2abae0f355d222ffc6bee4775f187ed46a4526a8
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-10-01 13:25:15 +0530

    fix: resource fetching with auto failing for the first request
    - setup frappe-ui resource fetcher before router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,16 +3,15 @@ import "./index.css"
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 import studio_router from "@/router/studio_router"
+import "./setupFrappeUIResource"
 import app_router from "@/router/app_router"
 import App from "./App.vue"
 
-import { setConfig, frappeRequest, resourcesPlugin, FeatherIcon } from "frappe-ui"
+import { resourcesPlugin, FeatherIcon } from "frappe-ui"
 import { registerGlobalComponents } from "./globals"
 
 const studio = createApp(App)
 const pinia = createPinia()
-
-setConfig("resourceFetcher", frappeRequest)
 
 // For the main app builder
 studio.use(studio_router)
