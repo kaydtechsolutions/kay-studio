32c4e185876da671fa4d7a97db0efe5ba63ceb67
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-10-02 21:56:03 +0530

    fix: bottom tabs style & routing

--- frontend/src/components/AppLayout/BottomTabs.vue
+++ frontend/src/components/AppLayout/BottomTabs.vue
@@ -1,10 +1,10 @@
 <template>
-	<nav class="sticky bottom-0 z-[1000] h-15 w-full border-t border-gray-200 bg-white shadow-lg">
+	<nav class="fixed bottom-0 z-[1000] h-15 w-full border-t border-gray-200 bg-white shadow-lg">
 		<div class="flex items-center justify-around">
 			<router-link
 				v-for="(tab, index) in tabs"
 				:key="index"
-				:to="tab.route"
+				:to="{ path: tab.route }"
 				class="flex flex-col items-center px-4 py-2 text-gray-600 hover:text-gray-800"
 				:class="[$router.currentRoute.value.path === tab.route ? 'text-gray-800' : '']"
 			>
