e54df788e7931e798e184db6a613e9f6570794dd
Rucha Mahabal <ruchamahabal2@gmail.com>
2024-12-12 07:43:46 +0530

    refactor: slot addition UI
    - skip already added slots

--- frontend/src/components/ComponentProps.vue
+++ frontend/src/components/ComponentProps.vue
@@ -1,7 +1,11 @@
 <template>
 	<div class="flex select-none flex-col pb-16">
 		<div v-if="props.block?.componentName && !props.block?.isRoot()" class="flex flex-col gap-3">
-			<CollapsibleSection sectionName="Props">
+			<!-- props -->
+			<div class="flex items-center justify-between text-sm font-medium">
+				<h3 class="cursor-pointer text-base text-gray-900">Props</h3>
+			</div>
+			<div class="mb-4 mt-3 flex flex-col gap-3">
 				<div v-for="(config, propName) in componentProps" :key="propName">
 					<InlineInput
 						:label="propName"
@@ -12,11 +16,23 @@
 						@update:modelValue="(val) => updateComponentProp(propName, val)"
 					/>
 				</div>
-			</CollapsibleSection>
+			</div>
 
-			<CollapsibleSection sectionName="Slots" v-if="componentSlots.length !== 0">
+			<!-- slots -->
+			<div class="mt-3 flex items-center justify-between text-sm font-medium">
+				<h3 class="cursor-pointer text-base text-gray-900">Slots</h3>
+				<Autocomplete
+					:options="componentSlots"
+					@update:modelValue="(slot: SelectOption) => block?.addSlot(slot.value)"
+				>
+					<template #target="{ togglePopover }">
+						<Button @click="togglePopover" size="sm" variant="ghost" icon="plus" />
+					</template>
+				</Autocomplete>
+			</div>
+
+			<div class="mb-4 mt-3 flex flex-col gap-3" v-if="!isObjectEmpty(block?.componentSlots)">
 				<div
-					v-if="!isObjectEmpty(block?.componentSlots)"
 					v-for="(slot, name) in block?.componentSlots"
 					:key="name"
 					class="flex w-full flex-row justify-between"
@@ -31,18 +47,9 @@
 						<Button variant="outline" size="sm" icon="x" @click="block?.removeSlot(name)" />
 					</div>
 				</div>
+			</div>
 
-				<EmptyState v-else message="No slots added" />
-
-				<Autocomplete
-					:options="componentSlots"
-					@update:modelValue="(slot: SelectOption) => block?.addSlot(slot.value)"
-				>
-					<template #target="{ togglePopover }">
-						<Button @click="togglePopover" class="w-full">Add</Button>
-					</template>
-				</Autocomplete>
-			</CollapsibleSection>
+			<EmptyState v-else message="No slots added" />
 		</div>
 
 		<EmptyState v-else message="Select a block to edit properties" />
@@ -56,7 +63,6 @@ import Block from "@/utils/block"
 import { getComponentProps, getComponentSlots } from "@/utils/components"
 import InlineInput from "@/components/InlineInput.vue"
 import EmptyState from "@/components/EmptyState.vue"
-import CollapsibleSection from "@/components/CollapsibleSection.vue"
 import type { SelectOption } from "@/types"
 import { isObjectEmpty } from "@/utils/helpers"
 
@@ -89,9 +95,25 @@ const componentSlots = ref<string[]>([])
 watch(
 	() => props.block?.componentName,
 	async () => {
-		if (!props.block || props.block.isRoot()) return
-		const slots = await getComponentSlots(props.block.componentName)
-		componentSlots.value = slots.map((slot) => slot.name)
+		await updateAvailableSlots()
 	},
 )
+
+watch(
+	() => props.block?.componentSlots,
+	async () => {
+		await updateAvailableSlots()
+	},
+	{ deep: true },
+)
+
+const updateAvailableSlots = async () => {
+	if (!props.block || props.block.isRoot()) return
+
+	const slots = await getComponentSlots(props.block.componentName)
+	// filter out already added slots
+	componentSlots.value = slots
+		.filter((slot) => !(slot.name in (props.block?.componentSlots || [])))
+		.map((slot) => slot.name)
+}
 </script>
