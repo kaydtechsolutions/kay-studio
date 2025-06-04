d47fa81bac135856b7f818b1858cc7b729ac8ea5
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-06-04 13:05:45 +0530

    fix: allow all hosts to send requests to vite dev server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
 		// Required for the app renderer running on webserver port
 		// https://vite.dev/guide/backend-integration
 		origin: "http://127.0.0.1:8080",
+		allowedHosts: true,
 	},
 	plugins: [
 		frappeui({
