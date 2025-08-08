405698b41db813b33155e9d97b01ccf614b68e1e
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-08-08 18:03:00 +0530

    fix: overflow should be visible in FitContainer
    - text fields shadow gets hidden if wrapped in a container

--- frontend/src/components/AppLayout/FitContainer.vue
+++ frontend/src/components/AppLayout/FitContainer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex h-fit w-fit shrink-0 flex-col overflow-hidden">
+	<div class="flex h-fit w-fit shrink-0 flex-col overflow-visible">
 		<slot></slot>
 	</div>
 </template>
