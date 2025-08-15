a8957a2514451e94980fbc86c87b29c67c3335b8
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-08-16 02:06:17 +0530

    fix: line height for empty state message

--- frontend/src/components/EmptyState.vue
+++ frontend/src/components/EmptyState.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="flex flex-col items-center rounded border-2 border-dashed border-gray-300 p-5 text-center text-sm text-gray-600"
+		class="flex flex-col items-center rounded border-2 border-dashed border-gray-300 p-5 text-center text-sm leading-5 text-gray-600"
 	>
 		{{ message }}
 	</div>
