2183a95f44c7ebc2bc1d03590852b0f12cb21882
Rucha Mahabal <ruchamahabal2@gmail.com>
2025-08-17 11:23:46 +0530

    feat: Studio Component doctype

---
+++ studio/studio/doctype/studio_component/studio_component.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2025, Frappe Technologies Pvt Ltd and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class StudioComponent(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		blocks: DF.JSON | None
+		component_id: DF.Data | None
+		component_name: DF.Data | None
+	# end: auto-generated types
+
+	def before_insert(self):
+		if not self.component_id:
+			self.component_id = frappe.generate_hash(length=16)
